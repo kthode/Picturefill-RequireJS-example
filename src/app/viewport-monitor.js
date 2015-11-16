b163c5b65882a56f844a3723fe263852b9311e4e
Kenneth Thode <k.thode@gmail.com>
2015-11-16 22:22:10 +0100

    refactored viewport-monitor module

--- src/app/viewport-monitor.js
+++ src/app/viewport-monitor.js
@@ -1,12 +1,39 @@
 define(['jquery'], function ($) {
 
-    var exports = function(element,options) {
-        var viewport = $(element);
-        $(window).on("resize", function() {
-            viewport.text($(this).width());
-        }).resize();
-        // the element parameter contains a reference
-        // to the '<a data-module="IMap"> ... </a>' node
+    var exports = function(element, options) {
+        this._element = element;
+        this._options = options || {};
+        this._initialize();
+    };
+    
+    exports.prototype = {
+
+        /**
+         * Initialize module.
+         *
+         * @memberof 
+         * @static
+         * @private
+         */
+        _initialize: function () {
+            console.log('Initialized app/viewport-monitor with options', this._options, 'on', new Date());
+            var _this = this;
+            $(window).on("resize", function() {
+                $(_this._element).text($(this).width());
+            }).resize();
+        },
+
+        /**
+         * Clean up when unloading this module.
+         *
+         * @memberof 
+         * @static
+         * @public
+         */
+        unload: function () {
+
+        }
+
     };
     return exports;
 });
